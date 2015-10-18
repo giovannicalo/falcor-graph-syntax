bb3263911e30d7dd6da92d55f49e45ea762aa6cd
Giovanni Calò <giovanni.calo@live.it>
2015-10-18 03:13:09 +0100

    Fixed style issue in the JavaScript client Webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,13 @@ module.exports = {
 		"webpack/hot/only-dev-server",
 		"./source/index.js"
 	],
-	module: { loaders: [{
-		exclude: /node_modules/,
-		loaders: ["react-hot", "babel?stage=0"],
-		test: /\.js$/
-	}] },
+	module: {
+		loaders: [{
+			exclude: /node_modules/,
+			loaders: ["react-hot", "babel?stage=0"],
+			test: /\.js$/
+		}]
+	},
 	output: {
 		filename: "main.js",
 		path: "./www",
